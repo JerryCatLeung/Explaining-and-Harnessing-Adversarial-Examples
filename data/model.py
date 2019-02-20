362a6fb4530d841235afd08d2b87cee3542adca7
JerryCatLeung <lp15136586397@live.cn>
2019-02-20 16:58:00 +0800

    build a model

---
+++ data/model.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+@project:Explaining-and-Harnessing-Adversarial-Examples
+@author: Kevin Leung
+@file:model.py
+@interpreter: Python3.6
+@ide:PyCharm
+@time:2019-02-20 16:57:12
+"""
