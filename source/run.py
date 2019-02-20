ab42edfc222d20053baf8f2d83b186e01237207b
JerryCatLeung <lp15136586397@live.cn>
2019-02-20 17:20:32 +0800

    understanding tensorflow.slice()

--- source/run.py
+++ source/run.py
@@ -8,3 +8,14 @@
 @ide:PyCharm
 @time:2019-02-20 16:58:49
 """
+import numpy as np
+import tensorflow as tf
+
+in_put = np.random.random(100)
+in_put = np.reshape(in_put, (5, 5, 4))
+out = tf.slice(in_put, [0, 2, 0], [1, -1, -1])
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+    print(sess.run(out))
